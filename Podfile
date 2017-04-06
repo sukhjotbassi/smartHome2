76c67562457a4f47bd9898184418cc19f518a869
Sukhjot Bassi <sukhjotbassi@gmail.com>
2017-04-05 18:18:14 -0700

    Updates with AmazonS3 (Basic UI)

--- Podfile
+++ Podfile
@@ -1,13 +1,10 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+#platform :ios, '9.0'
+
+use_frameworks!
 
 target 'SmartHomeiOS' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
-  use_frameworks!
-
-  # Pods for SmartHomeiOS
-
   pod 'Firebase/Core'
   pod 'Firebase/Auth'
-
+  pod 'AWSS3', '~> 2.5.0'
 end
--- Pods/Target Support Files/Pods-SmartHomeiOS/Pods-SmartHomeiOS-frameworks.sh
+++ Pods/Target Support Files/Pods-SmartHomeiOS/Pods-SmartHomeiOS-frameworks.sh
@@ -89,10 +89,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/AWSCore/AWSCore.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/AWSS3/AWSS3.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/AWSCore/AWSCore.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/AWSS3/AWSS3.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
 fi
